fcb28dee9d649c693ddb4e648d2f89409d2e1b1e
sean <1963966513@qq.com>
2021-06-01 18:22:24 +0800

    add type for rem.ts

--- src/config/rem.ts
+++ src/config/rem.ts
@@ -1,13 +1,13 @@
-function adapt(designWidth: number, rem2px: number){
-  var d = window.document.createElement('div');
+function adapt(designWidth: number, rem2px: number): number{
+  let d:HTMLElement = window.document.createElement('div');
   d.style.width = '1rem';
   d.style.display = "none";
-  var head = window.document.getElementsByTagName('head')[0];
+  let head = window.document.getElementsByTagName('head')[0];
   head.appendChild(d);
-  var defaultFontSize = parseFloat(window.getComputedStyle(d, null).getPropertyValue('width'));
-  var st = document.createElement('style');
-  var portrait = "@media screen and (min-width: "+window.innerWidth+"px) {html{font-size:"+ ((window.innerWidth/(designWidth/rem2px)/defaultFontSize)*100) +"%;}}";
-  var landscape = "@media screen and (min-width: "+window.innerHeight+"px) {html{font-size:"+ ((window.innerHeight/(designWidth/rem2px)/defaultFontSize)*100) +"%;}}"
+  let defaultFontSize = parseFloat(window.getComputedStyle(d, null).getPropertyValue('width'));
+  let st = document.createElement('style');
+  let portrait = "@media screen and (min-width: "+window.innerWidth+"px) {html{font-size:"+ ((window.innerWidth/(designWidth/rem2px)/defaultFontSize)*100) +"%;}}";
+  let landscape = "@media screen and (min-width: "+window.innerHeight+"px) {html{font-size:"+ ((window.innerHeight/(designWidth/rem2px)/defaultFontSize)*100) +"%;}}"
   st.innerHTML = portrait + landscape;
   head.appendChild(st);
   return defaultFontSize
